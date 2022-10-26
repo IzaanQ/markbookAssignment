67e5227f44b0d83279254ce334b4b916fa52255b
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2022-10-26 11:37:22 -0400

    Add files via upload

---
+++ StudentProfileDefault.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class StudentProfileDefault
+    {
+        public string name;
+        public int grade;
+        public int numberOfClasses = 4;
+        public string subject1;
+        public int subject1Grade;
+        public string subject2;
+        public int subject2Grade;
+        public string subject3;
+        public int subject3Grade;
+        public string subject4;
+        public int subject4Grade;
+        public int averageGrade;
+    }
+}
